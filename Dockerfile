2f8eab9c8899559b7fa26434d5598f79dc47cd5e
Kustex <lucaskustermans@outlook.com>
2023-11-02 10:26:01 +0100

    Initial commit: Set up Flask application with Docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10.12
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install the required Python packages
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port the app runs on
+EXPOSE 5000
+
+# Define the command to run your application
+CMD ["python", "app.py"]
+
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import pandas as pd
+import yfinance as yf
+import plotly.express as px
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Fetch SPX data using yfinance
+    spx_data = yf.download('^GSPC', start='2022-01-01', end='2023-01-01')
+
+    # Create an interactive plot using Plotly
+    fig = px.line(spx_data, x=spx_data.index, y='Close', title='S&P 500 (SPX) Stock Chart')
+    graphJSON = fig.to_json()
+
+    return render_template('index.html', graphJSON=graphJSON)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
